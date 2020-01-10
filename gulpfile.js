b0de861056c16eeb291061f937c0b40909c9e16f
Сергей Дербенцев <modcom@rambler.ru>
2020-01-10 18:00:23 +0300

    upd

--- gulpfile.js
+++ gulpfile.js
@@ -74,9 +74,9 @@ gulp.task("css", function () {
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([autoprefixer()]))
-    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sourcemap.write("."))
     .pipe(server.stream());
 });
