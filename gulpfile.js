1b2bed47ea8d85a2fad5663f20da6e5744106c70
Сергей Дербенцев <modcom@rambler.ru>
2020-01-10 10:10:43 +0300

    Сделал пиксели в index на мобильной, планшетной версии

--- gulpfile.js
+++ gulpfile.js
@@ -74,10 +74,10 @@ gulp.task("css", function () {
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([autoprefixer()]))
+    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
