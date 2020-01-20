6d5592fe8b53d2134045731aa1176ac707c2d738
Сергей Дербенцев <modcom@rambler.ru>
2020-01-20 17:10:46 +0300

    upd

--- source/js/header-menu.js
+++ source/js/header-menu.js
@@ -1,7 +1,11 @@
 var navMain = document.querySelector(".header__nav");
 var navButton = document.querySelector(".header__button");
 
-navMain.classList.remove("header__nav--nojs");
+/* navMain.classList.remove("header__nav--nojs"); */
+navMain.classList.remove("header__nav--opened");
+navMain.classList.add("header__nav--closed");
+
+navButton.classList.remove("header__button-nojs");
 
 navButton.addEventListener("click", function () {
   if (navMain.classList.contains("header__nav--closed")) {
